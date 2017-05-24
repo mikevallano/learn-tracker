e733f9dbda91e951bc586d7323c1b2300cf4e01b
mike vallano <mikevallano@gmail.com>
2017-05-23 19:22:51 -0500

    Initial Commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
   }
 
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => 'http://i-learned-that.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'http://learn-tracker.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
